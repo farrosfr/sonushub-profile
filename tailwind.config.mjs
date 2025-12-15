42e7e6c58dc3657a6028ad72eb5017c06073687b
Mochammad Farros Fatchur Roji <farros.alfatih@gmail.com>
2025-12-15 11:39:56 +0700

    feat: hero

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,10 +3,25 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-    screens: {
-      'max-desktop': '1440px',
-    }
-  },
+      screens: {
+        'max-desktop': '1440px',
+      },
+      keyframes: {
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(20px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
+      }
+    },
   },
   plugins: [],
 }
